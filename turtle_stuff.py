3ef6dd85f78793b61bafe1f8d0adfe65a21c924b
Khaledalnatour <143716760+Khaledalnatour@users.noreply.github.com>
2023-09-12 15:59:13 +0400

    full version

--- turtle_stuff.py
+++ turtle_stuff.py
@@ -1,5 +1,5 @@
 import turtle
-def test_drive():
+'''def test_drive():
     turtle.forward (100)
     turtle.left (87)
     turtle.setheading (127)
@@ -19,4 +19,28 @@ def main():
     test_drive()
     input ("press enter to continue")
     turtle_state()
-main()
+main()'''
+def square(a,b,c):
+    turtle.pensize(4)
+    turtle.pencolor("red")
+    turtle.fillcolor("blue")
+    turtle.begin_fill()
+    for i in range (4):
+        turtle.forward(a)
+        turtle.left(90)
+    for i in range(4):
+        turtle.forward(b)
+        turtle.left(90)
+    for i in range(4):
+        turtle.forward(c)
+        turtle.left(90)
+    turtle.end_fill()
+    turtle.bgcolor("pink")
+    turtle.done()
+    
+
+a = int(input("enter the value:"))
+b = int(input("enter the value:"))
+c = int(input("enter the value:"))
+
+square(a,b,c)
